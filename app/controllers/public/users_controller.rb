eef55a6f38c99f62b3bf1a362c0ec52daec79f64
dmmyuki <srkwyuki28@gmail.com>
2022-09-05 14:06:30 +0000

    [Create]会員機能[Add]トップ/アバウト

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,12 +9,22 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_customer
+    @user.update(user_params)
+    redirect_to user_my_page_path(current_customer)
   end
 
   def suspended
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :profile_image, :nickname, :phone_number, :email)
   end
 
 end
