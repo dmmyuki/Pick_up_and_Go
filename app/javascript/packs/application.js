54d6978c11f28cc0c71c13f72eb4cf2357edfef9
dmmyuki <srkwyuki28@gmail.com>
2022-10-01 13:43:54 +0000

    [Create]Skippr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'jquery'
 import "skippr.min"
-import "script.js"
-
-import "jquery";
+import "script.js";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
