910cf3c6c4c772b3f5b7d47472eafd412e9cba41
dmmyuki <srkwyuki28@gmail.com>
2022-10-31 10:54:49 +0000

    [Add]バリデーションチェック修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ApplicationRecord
   # addressカラムを基準に緯度経度を算出する。
   geocoded_by :address
   # 住所変更時に緯度経度も変更する。
-  after_validation :geocode
+  before_validation :geocode
 
   def get_image
     unless image.attached?
