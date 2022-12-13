e9319e8c1d722593bb685ebf74b7c7859937ca09
dmmyuki <srkwyuki28@gmail.com>
2022-12-13 13:41:53 +0000

    [Add]homes/top いいねランキング

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,7 @@ class Public::HomesController < ApplicationController
   def top
     @user = current_user
     @places = Post.order('id DESC').limit(4)
+    @place_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def about
